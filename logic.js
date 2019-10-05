97bd5b90dcb5c635a14939d2e110938706f191c6
Omer Katan <omerkatan@live.com>
2019-10-05 00:00:42 -0700

    All the javascript aspects are done

--- logic.js
+++ logic.js
@@ -1,91 +1,94 @@
-
-
-
 var passwordLength = prompt("Choose the length of your password");
 var passwordLengthInt = parseInt(passwordLength);
-// var characterTypeSpecial = confirm("Press OK if you would like any special characters in your password");
-// var characterTypeNumeric = confirm("Press OK if you would like any numbers in your password");
-// var characterTypeUppercase = confirm("Press OK if you would like any uppercase characters");
-// var characterTypeLowercase = confirm("Press OK if you would like any lowercase characters");
 
 
-
-if (passwordLengthInt > 8 && passwordLengthInt < 128) {
-
-
-
-var characterTypeSpecial = confirm("Press OK if you would like any special characters in your password");
-var characterTypeNumeric = confirm("Press OK if you would like any numbers in your password");
-var characterTypeUppercase = confirm("Press OK if you would like any uppercase characters");
-var characterTypeLowercase = confirm("Press OK if you would like any lowercase characters");
+// this if statement checks if the password prompted is between 8 and 128 characters
+if (passwordLengthInt >= 8 && passwordLengthInt <= 128) {
 
 
-
-var character = {
-    "special": ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"],
-    "numeric": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
-    "uppercase": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    "lowercase": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-}
-
-var generatorMixType = [];
-function setTypes() {
-
-    var generatorSpecial = Math.floor(Math.random() * character.special.length);
-    var generatorNumeric = Math.floor(Math.random() * character.numeric.length);
-    var generatorUppercase = Math.floor(Math.random() * character.uppercase.length);
-    var generatorLowercase = Math.floor(Math.random() * character.lowercase.length);
-
-    var s = character.special[generatorSpecial];
-    var n = character.numeric[generatorNumeric];
-    var u = character.uppercase[generatorUppercase];
-    var l = character.lowercase[generatorLowercase];
+    var characterTypeSpecial = confirm("Press OK if you would like any special characters in your password");
+    var characterTypeNumeric = confirm("Press OK if you would like any numbers in your password");
+    var characterTypeUppercase = confirm("Press OK if you would like any uppercase characters");
+    var characterTypeLowercase = confirm("Press OK if you would like any lowercase characters");
 
 
-    if (characterTypeSpecial) {
-        generatorMixType.push(s);
+    if (characterTypeSpecial === false && characterTypeNumeric === false && characterTypeUppercase === false && characterTypeLowercase === false) {
+        alert("You must choose at least one character type, refresh the page to reset the program");
     }
-    if (characterTypeNumeric) {
-        generatorMixType.push(n);
+
+
+    var character = {
+        "special": ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"],
+        "numeric": ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        "uppercase": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+        "lowercase": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     }
-    if (characterTypeUppercase) {
-        generatorMixType.push(u);
-    }
-    if (characterTypeLowercase) {
-        generatorMixType.push(l);
-    }
-    return generatorMixType;
-}
 
 
-
-
-function generatePassword(generatorMixType) {
+    var generatorMixType = [];
     var result = '';
 
 
 
-    for (var i = 0; i < passwordLengthInt; i++) {
+    function generatePassword() {
+
+        for (var i = 0; i < passwordLengthInt; i++) {
+            var generatorSpecial = Math.floor(Math.random() * character.special.length);
+            var generatorNumeric = Math.floor(Math.random() * character.numeric.length);
+            var generatorUppercase = Math.floor(Math.random() * character.uppercase.length);
+            var generatorLowercase = Math.floor(Math.random() * character.lowercase.length);
+
+            var arraySpecial = character.special[generatorSpecial];
+            var arrayNumeric = character.numeric[generatorNumeric];
+            var arrayUppercase = character.uppercase[generatorUppercase];
+            var arrayLowercase = character.lowercase[generatorLowercase];
 
 
+            if (characterTypeSpecial) {
+                generatorMixType.push(arraySpecial);
+            }
+            if (characterTypeNumeric) {
+                generatorMixType.push(arrayNumeric);
+            }
+            if (characterTypeUppercase) {
+                generatorMixType.push(arrayUppercase);
+            }
+            if (characterTypeLowercase) {
+                generatorMixType.push(arrayLowercase);
+            }
 
-        var generatorMixRandom = generatorMixType[Math.floor(Math.random() * generatorMixType.length)];
-        console.log(generatorMixRandom);
+            var mixArrayType = generatorMixType[Math.floor(Math.random() * generatorMixType.length)];
+            var mixArrayTypeString = mixArrayType + '';
 
+            result += mixArrayTypeString;
+        }
 
-
-        result += generatorMixRandom;
+        return result;
     }
-    return result;
-}
 
 
-// This replaces the placeholder text and puts the password in the html body
-function getPassword() {
-    document.getElementById("passwordHolder").innerHTML = generatePassword(setTypes());
-}
+
+    // This replaces the placeholder text and puts the password in the html body
+    function getPassword() {
+        document.getElementById("passwordHolder").innerHTML = generatePassword();
+        result = '';
+    }
+
 
 }
 else {
     alert("Password must be between 8 and 128 characters, refresh the page to reset the program")
+}
+
+
+// this uses the window object to get copy the passwordHolder ID to clipboard
+function copyPassword(passwordHolder) {
+    var getPassword = document.getElementById(passwordHolder);
+    var range = document.createRange();
+    window.getSelection().removeAllRanges();
+    range.selectNode(getPassword);
+    window.getSelection().addRange(range);
+    document.execCommand('copy');
+    window.getSelection().removeAllRanges();
+    alert("Your Password has been copied");
 }
