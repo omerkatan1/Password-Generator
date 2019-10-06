6816c6d44fe6b4c6aeffea3b80875b14ea7230fa
Omer Katan <omerkatan@live.com>
2019-10-05 20:41:16 -0700

    final draft - typo on last save

--- logic.js
+++ logic.js
@@ -59,6 +59,7 @@ function generatePassword() {
                 generatorMixType.push(arrayLowercase);
             }
 
+
             var mixArrayType = generatorMixType[Math.floor(Math.random() * generatorMixType.length)];
             var mixArrayTypeString = mixArrayType + '';
 
@@ -78,8 +79,8 @@ function generatePassword() {
     if (characterTypeSpecial === false && characterTypeNumeric === false && characterTypeUppercase === false && characterTypeLowercase === false) {
         alert("You must choose at least one character type");
     }
-    if (passwordLengthInt <= 8 || passwordLengthInt >= 128) {
-        alert("Password must be between 8 and 128 characters")
+    if (passwordLengthInt < 8 || passwordLengthInt > 128) {
+        alert("Password must be between 8 and 128 characters");
     }
 }
 
